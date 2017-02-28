3344bac622f7d4d6e2e8b42c6f6d3076ca58fd3f
sec1412 <Andreas.Hoepcke@haw-hamburg.de>
2017-02-28 16:21:30 +0100

    Add Gamecontroller for Score & Basketball prefab

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public TextMesh scoreText;
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+
+		score = 0;
+		UpdateScore();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider col) {
+    if (col.gameObject.tag == "Target") {
+			AddScore(20);
+		}
+	}
+
+	public void AddScore (int scoreValue) {
+		score += scoreValue;
+		UpdateScore();
+
+	}
+
+	void UpdateScore () {
+		scoreText.text = "Score: " + score;
+	}
+}
