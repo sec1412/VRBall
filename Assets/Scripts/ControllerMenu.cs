dc6437d82b6fc09c21a4e7bf71063dd258ab1e00
sec1412 <Andreas.Hoepcke@haw-hamburg.de>
2017-03-01 15:21:24 +0100

    Add Radial Menu to left Controller with basketball spawn function (max 5)

---
+++ Assets/Scripts/ControllerMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMenu : MonoBehaviour {
+
+	public GameObject basketball;
+	private int ballCount;
+	private GameObject instObj;
+
+
+	// Use this for initialization
+	void Start () {
+		ballCount = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SpawnBall () {
+		if (ballCount != 5) {
+		instObj = Instantiate (basketball, transform.position+(transform.forward*2), transform.rotation);
+		ballCount += 1;
+		}
+	}
+}
